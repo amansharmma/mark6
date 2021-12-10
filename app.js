6686858ddbe8881b612274344f926ccc8f44860a
amansharmma <aman18@navgurukul.org>
2021-12-10 10:39:22 +0530

    uo

---
+++ app.js
@@ -0,0 +1,58 @@
+// var btntranslate= document.querySelector('#btn-translate');
+// var txtarea=document.querySelector('#txt-area');
+// var output=document.querySelector('#output');
+
+// var baseURL="https://api.funtranslations.com/translate/minion.json";
+// function makeURL(input){
+    
+//     return baseURL+"?"+"text="+input;
+// }
+// function errorHandler(error){
+//     console.log("error occured ",error);
+// }
+// btntranslate.addEventListener("click",function clickHandler(){
+//     console.log(txtarea.value)
+//     fetch(makeURL(txtarea.value))
+//       .then(response => response.json())
+//       .then(json => {
+//           var translated=json.contents.translated;
+//           output.innerText=translated;
+//       })
+//       .catch(errorHandler)
+// })
+
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-area");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+outputDiv.innerText="show my language"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler(){
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
